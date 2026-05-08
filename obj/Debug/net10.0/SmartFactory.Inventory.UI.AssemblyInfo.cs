055ebef8d91d5e123c1b93e3daec79e25c52d6e8
Felix Dombrowsky <feldombr@deltaww.com>
2026-05-08 19:45:06 +0700

    Start on Work Order Page

--- obj/Debug/net10.0/SmartFactory.Inventory.UI.AssemblyInfo.cs
+++ obj/Debug/net10.0/SmartFactory.Inventory.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70d3f9b7d6236461215c06a9e26c612d4426e58d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb9991cfdd7cdee467641411842cf8aeb8807e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

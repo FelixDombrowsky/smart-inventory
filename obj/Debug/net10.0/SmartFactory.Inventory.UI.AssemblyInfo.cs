13282523278ebe81c95f387c70d01a4b996992e9
Felix Dombrowsky <feldombr@deltaww.com>
2026-06-08 19:08:52 +0700

    work_order_lot_graph

--- obj/Debug/net10.0/SmartFactory.Inventory.UI.AssemblyInfo.cs
+++ obj/Debug/net10.0/SmartFactory.Inventory.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a6aae3f77a6e6a37d2dd86284584b388fb81b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f86d340af18107a08282bf06a01757c0f2a7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

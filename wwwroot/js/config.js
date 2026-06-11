1d58e25477166bbb65810f6312796e620d056af4
Felix Dombrowsky <feldombr@deltaww.com>
2026-06-11 15:28:51 +0700

    Add role-based nav restrictions, docker config, receive page tweaks

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -11,7 +11,12 @@ INVENTORY_API : "https://10.204.32.135:5207"
 
 }*/
 
+// const CONFIG = {
+//     AUTH_API : "http://10.204.212.28:15000",
+//     INVENTORY_API : "http://10.204.212.28:5207"
+// };
+
 const CONFIG = {
-    AUTH_API : "http://10.204.212.28:15000",
-    INVENTORY_API : "http://10.204.212.28:5207"
+    AUTH_API : "",
+    INVENTORY_API : "/api"
 };

5c5c91f562b681a3da80d49649cc81e180104730
Felix Dombrowsky <feldombr@deltaww.com>
2026-07-16 16:07:15 +0700

    ui-inventory-v1.0.1.4 Add new field - {CreatedAt} in Print Layout

--- obj/Debug/net10.0/SmartFactory.Inventory.UI.AssemblyInfo.cs
+++ obj/Debug/net10.0/SmartFactory.Inventory.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7af8d533f549097af0b3e4d90d05c11799c86c9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a81f6c8cb6f44199e54c26aeb28b9971bc49b3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartFactory.Inventory.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
